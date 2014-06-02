4d803dfa88714e929f669436d8ed2232c96dc91f
Rafael Chaves <1120677@isep.ipp.pt>
2014-06-02 18:03:25 +0100

    Fixed issue #200

---
+++ src/csheets/userstories/us055/package-info.java
@@ -0,0 +1,17 @@
+/**
+ * Technical documentation regarding the user story 055: Cell's Events. 
+ * <br/>
+ * <br/>
+ * 
+ * <b>Requirement</b><br/>
+ * Setup events on cells. The user can then choose, with the help of an graphic interface, which events are going to be logged. Every time said event occurs a new line in the log file has to be added with the event's name.
+ * <br/>
+ * <br/>
+ *  
+ * <b>S001a: Analysis</b><br/>
+ * Since events on cells are 
+ * <br/>
+ * <br/>
+ * 
+ **/
+package csheets.userstories.us055;
