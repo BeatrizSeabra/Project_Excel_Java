a912d55706a39590bee429cd89d3197d09099e09
diogomoreira <1120339@isep.ipp.pt>
2014-06-02 17:33:32 +0100

    fixed issue #143

--- src/csheets/userstories/us025/package-info.java
+++ src/csheets/userstories/us025/package-info.java
@@ -4,15 +4,16 @@
  * <br/>
  * 
  * <b>Requirement</b><br/>
- * Criação de uma componente que permite exportar o conteúdo de uma cleanSheets para xml.
+ *  Deve ser possível exportar o conteúdo de uma folha para um ﬁcheiro XML.Apenas o conteúdo das células deve ser exportado (i.e., não é necessário exportar as formulas, etc.).
+
  * <br/>
  * <br/>
  *  
  * <b>S025a: Analysis</b><br/>
  * A implementação deste use case irá permitir exportar o conteúdo de uma cleanSheets para xml, para tal é necessário criar uma interface que permitar controlar as exportações de documentos em diferentes formatos, seguindo o padrão strategy.<br/> 
  * A classe responsável pela exportação em xml irá implementar essa inteface.<br/>
- * No menuBar será acrescentado uma opção chamada Export. Uma das opções do subMenu será xml.<br/>
- * Com essa opção adicionada ao menur será gerardo, ao selecionar essa opção, uma instancia da classe exportXmlUI, responsável pela troca de dados entre utilizador e sistema.<br/>
+ * Será acrescentado ao save uma opção de guradar como xml .<br/>
+ * Com essa opção adicionada, ao ser selecionar, uma instancia da classe exportXmlUI, responsável pela troca de dados entre utilizador e sistema.<br/>
  * Após a troca de dados com a exportXmlUI uma instancia do controlExportXML será criada e essa ira instanciar a classe ExportXML e a informação contida no objecto da classe Workbook será exportada para xml.<br/>
  * <br/>
  * 
