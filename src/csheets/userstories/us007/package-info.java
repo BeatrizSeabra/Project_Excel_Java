286b173f14af6d29dfdc3b17ab2213a0ae2bbb30
stefanteixeira <1120739@isep.ipp.pt>
2014-06-02 17:40:46 +0100

    Foi adicionada uma nova package ao projeto com uma classe para modificar o javadoc do US007 close #201

---
+++ src/csheets/userstories/us007/package-info.java
@@ -0,0 +1,21 @@
+/**
+ * Technical documentation regarding the user story 007: sort cells. 
+ * <br/>
+ * <br/>
+ * 
+ * <b>Requirement</b><br/>
+ * Sort by order the cells content. For this use case only need to sort one collumn.
+ * <br/>
+ * <br/>
+ *  
+ * <b>S001a: Analysis</b><br/>
+ * 
+ * Since the cells will be filled with strings and number, they will need to be sort by order. The user will be able to choose how he/she want to order the collumn.
+ * This will be added to the submenu cell decorators on view menu that will show various types of order.
+ * The class method that will be called is SortAction() that creates a new sorting action and then will use the actionPerformed method that will allow the user to choose the criteria wanted. 
+ * Then will be called a new method that contains the code for sorting all the collumn with the order type picked by the user. 
+ * <br/>
+ * <br/>
+ * */
+
+package csheets.userstories.us007;
