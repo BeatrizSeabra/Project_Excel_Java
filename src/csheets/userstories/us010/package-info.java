cce75d3cc5ed6d212115ded2e2a1004d6e69e497
oyermak <1120622@isep.ipp.pt>
2014-06-02 17:53:26 +0100

    package-info.java Relativo à análise do us010

---
+++ src/csheets/userstories/us010/package-info.java
@@ -0,0 +1,23 @@
+/**
+ * Technical documentation regarding the user story 010: activate and deactivate extensions. 
+ * <br/>
+ * <br/>
+ * 
+ * <b>Requirement</b><br/>
+ * Disponibilizar uma janela que permita activar e desactivar extensões do cleansheets.
+ * <br/>
+ * <br/>
+ *  
+ * <b>S010a: Analysis</b><br/>
+ * User Story 010 tem como principal objectivo organizar as extensões existentes na aplicação. Uma nova janela que possuirá
+ * uma lista de todas as extensões existentes na aplicação, indicando a frente o seu "estado" (Activo ou Desactivo), assim o utilizador pode seleccionar
+ * as extenções que posteriormente aparecerão no separador "Extensions" e desactivar outras que por sua vez serão omitidos dessa lista de extenções.
+ * Para o desenvolvimento dessa funcionalidade serão usadas as classes já existentes "ExtensionManager" e "Extension" que
+ * irão forncer a Lista de Extenções e as Propriedades de Extenção (nome, etc), correspondentemente. 
+ * <br/>
+ * <br/>
+ * 
+ * @author Oleh Yermak (1120622)
+ * */
+
+package csheets.userstories.us010;
