c9ebb68ca6debb5a5e17aa8c197cbf9cc47d55cf
Alexandre Braganca <atb@isep.ipp.pt>
2014-06-02 14:44:22 +0100

    actualizacao dos scripts sh para criarem os diretórios caso estes não existam. Atencao que os scripts bat não fazem isso!

--- scripts/makejar.sh
+++ scripts/makejar.sh
@@ -1,6 +1,18 @@
 #!/bin/sh
 echo Compiling...
-./build.sh
+sh ./build.sh
+
+if [ -e ../dist ]   &&  [ -f ../dist ]
+then  
+        rm ../dist 
+fi 
+
+if [ ! -d ../dist ]   
+then 
+        mkdir ../dist 
+fi 
+
+
 
 echo Copying resource files...
 cp -R ../src-resources/csheets ../tmp-build 
@@ -9,7 +21,7 @@ echo Creating archive...
 jar cmf makejar.mf ../dist/csheets.jar -C ../tmp-build csheets
 
 echo Copiar as dependencias
-cp ../lib/antlr-3.5.2-complete.jar ../dist/antlr-3.5.2-complete.jar
+cp ../lib/*.jar ../dist/
 
 # echo Removing temporary files...
 # rm -R jar
