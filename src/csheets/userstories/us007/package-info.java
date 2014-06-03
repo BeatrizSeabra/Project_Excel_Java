9ce80570f83d3c1db061fa9c1f0047056991609d
stefanteixeira <1120739@isep.ipp.pt>
2014-06-03 18:22:32 +0100

    Mudança na analise do US007 #fixes 201

--- src/csheets/userstories/us007/package-info.java
+++ src/csheets/userstories/us007/package-info.java
@@ -11,7 +11,7 @@
  * <b>S001a: Analysis</b><br/>
  * 
  * Since the cells will be filled with strings and number, they will need to be sort by order. The user will be able to choose how he/she want to order the collumn.
- * This will be added to the submenu cell decorators on view menu that will show various types of order.
+ * This will be added to the submenu Sort on Edit menu that will open a JFrame or a messageDialog to input the collumn to be order.
  * The class method that will be called is SortAction() that creates a new sorting action and then will use the actionPerformed method that will allow the user to choose the criteria wanted. 
  * Then will be called a new method that contains the code for sorting all the collumn with the order type picked by the user. 
  * <br/>
