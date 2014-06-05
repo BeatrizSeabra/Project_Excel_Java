919bec18f18260e69345ccd50af6650b7ee92e09
Adriano Leite <1110316@isep.ipp.pt>
2014-06-05 14:58:16 +0100

    ref issue #168

--- src/csheets/ext/exporttxt/ui/TxtAction.java
+++ src/csheets/ext/exporttxt/ui/TxtAction.java
@@ -6,46 +6,53 @@ import javax.swing.JOptionPane;
 
 import csheets.ui.ctrl.BaseAction;
 import csheets.ui.ctrl.UIController;
+import javax.swing.ImageIcon;
 import javax.swing.JFileChooser;
 import javax.swing.filechooser.FileNameExtensionFilter;
 
 /**
- * An action of the simple extension that exemplifies how to interact with the spreadsheet.
+ * An action of the simple extension that exemplifies how to interact with the
+ * spreadsheet.
+ *
  * @author Tiba
  */
 public class TxtAction extends BaseAction {
 
-	/** The user interface controller */
-	protected UIController uiController;
+    /**
+     * The user interface controller
+     */
+    protected UIController uiController;
 
-	/**
-	 * Creates a new action.
-	 * @param uiController the user interface controller
-	 */
-	public TxtAction(UIController uiController) {
-		this.uiController = uiController;
-	}
+    /**
+     * Creates a new action.
+     *
+     * @param uiController the user interface controller
+     */
+    public TxtAction(UIController uiController) {
+        this.uiController = uiController;
+    }
 
-	protected String getName() {
-		return "Ficheiro de texto";
-	}
+    protected String getName() {
+        return "Ficheiro de texto";
+    }
 
-	protected void defineProperties() {
-	}
+    protected void defineProperties() {
+    }
 
-	/**
-	 * A simple action that presents a confirmation dialog.
-	 * If the user confirms then the contents of the cell A1 of the current sheet are set to the string "Changed".
-	 * @param event the event that was fired
-	 */
-	 public void actionPerformed(ActionEvent e) {
+    /**
+     * A simple action that presents a confirmation dialog. If the user confirms
+     * then the contents of the cell sheet are set to the
+     *
+     * @param event the event that was fired
+     */
+     public void actionPerformed(ActionEvent e) {
         JFileChooser chooser = new JFileChooser();
+        chooser.setDialogTitle("Save as Txt");
         FileNameExtensionFilter filter = new FileNameExtensionFilter("TXT", "txt");
         chooser.setFileFilter(filter);
         int returnVal = chooser.showOpenDialog(null);
         if(returnVal == JFileChooser.APPROVE_OPTION) {
-            System.out.println("You saved this file: " + chooser.getSelectedFile().getName());
-    }
-        
+            System.out.println("You Saved this file: " + chooser.getSelectedFile().getName());
+        }
     }
 }
