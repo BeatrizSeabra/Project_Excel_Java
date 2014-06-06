c1d474cbf0a6d6868a4814469b207c5fd7170358
Adriano Leite <1110316@isep.ipp.pt>
2014-06-06 14:33:02 +0100

    #180 Versão2

--- src/csheets/userstories/us073/package-info.java
+++ src/csheets/userstories/us073/package-info.java
@@ -3,58 +3,74 @@
  * <br/>
  * <br/>
  *
- *<b>Requerimento</b><br/>
- *US073- Exportar ficheiro de texto. Deve ser possível exportar dados para um ficheiro de texto,
- *cujas colunas são divididas por ",". Os dados devem ser exportados a partir de uma célula que é identificada
- *como o canto superior esquerdo.
- *</br>
- *</br>
+ * <b>Requerimento</b><br/>
+ * US073- Exportar ficheiro de texto. Deve ser possível exportar dados para um
+ * ficheiro de texto, cujas colunas são divididas por ",". Os dados devem ser
+ * exportados a partir de uma célula que é identificada como o canto superior
+ * esquerdo.
+ * </br>
+ * </br>
  *
  * <b>S073a: Analysis</b><br/>
- *Tendo a necessidade de criar uma funcionalidade para a exportação de dados para ficheiros de texto, 
- *é necessário utilizar a class Codec como base, de modo a servir de class mãe para uma nova class que utilize 
- *o seu método write, para assim conseguir escrever. Será necessário também adicionar uma outra opção no menu guardar, 
- *com a possibilidade de guardar em ficheiro de texto.
+ * Tendo a necessidade de criar uma funcionalidade para a exportação de dados
+ * para ficheiros de texto, é necessário utilizar a class Codec como base, de
+ * modo a servir de class mãe para uma nova class que utilize o seu método
+ * write, para assim conseguir escrever. Será necessário também adicionar uma
+ * outra opção no menu guardar, com a possibilidade de guardar em ficheiro de
+ * texto.
  *
  *
  *
  *
  * <br/>
  * <br/>
- * 
+ *
  * <b>S073a: Design</b><br/>
- * 
- * <img src="../../../csheets/userstories/us001/doc-files/us073_design1.png"> 
+ *
+ * <img src="../../../csheets/userstories/us001/doc-files/us073_design1.png">
  * <br/>
  * <br/>
- * 
+ *
  * <b>S073c: Coding</b><br/>
- 
+ *
  * <br/>
  * <br/>
- * 
+ *
  * <b>S073u: Unit Tests</b><br/>
+ * Sabendo que todos os metodos sao do tipo void nao foi possivel realizar
+ * testes unitarios.
+ * <br/>
+ * <br/>
+ * <b>S028f: Functional Tests</b><br/>
+ * 1 - Correr o cleansheets <br/>
+ * 2 - Escrever nas celulas <br/>
+ * 3 - Ir ao Menu "Extensions" <br/>
+ * 5 - Seleccionar a opcao "Export" e em seguida "TXT" <br/>
+ * 6 - Escolher o caminho e o nome do ficheiro <br/>
+ * 7 - Clicar no botao "save". <br/>
  *
  *
  *
- /*
-*
- @startuml doc-files/us073_design1.png
- "uic:UIController" -> ExtensionManager : extensions = getExtensions()
-loop [for Extension ext: extensions]
-"uic:UIController" -> "extension:ImportExtensions" : uiExtension=getUIExtension(this)
-"extension:ImportExtensions" -> "uiExtension:UIExtensionImport" : new(extension,uic)
-"uiExtension:UIExtensionImport" -> "uiExtension:UIExtensionImport" : getMenu()
-"uiExtension:UIExtensionImport" -> "menu:ImportMenuAction" : new(uic)
-
-note over of ExtensionManager: Completar .....
-"uic:UIController" -> "uic:UIController" : uiExtensions.add(uiExtension)
-end
- @enduml
-*
+ *
+ *
+ *
+ *
+ * /*
+ *
+ * @startuml doc-files/us073_design1.png "uic:UIController" -> ExtensionManager
+ * : extensions = getExtensions() loop [for Extension ext: extensions]
+ * "uic:UIController" -> "extension:ImportExtensions" :
+ * uiExtension=getUIExtension(this) "extension:ImportExtensions" ->
+ * "uiExtension:UIExtensionImport" : new(extension,uic)
+ * "uiExtension:UIExtensionImport" -> "uiExtension:UIExtensionImport" :
+ * getMenu() "uiExtension:UIExtensionImport" -> "menu:ImportMenuAction" :
+ * new(uic)
+ *
+ * note over of ExtensionManager: Completar ..... "uic:UIController" ->
+ * "uic:UIController" : uiExtensions.add(uiExtension) end
+ * @enduml
+ * 
 */
- 
- 
 /**
  * @author 1110316
  *
