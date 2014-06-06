cee5e791726f4b41bc99219ed9653f9b8f60c188
1120606 <1120606@isep.ipp.pt>
2014-06-06 14:08:54 +0100

    Package.info ref #186

--- src/csheets/ext/image/ui/InsertImageAction.java
+++ src/csheets/ext/image/ui/InsertImageAction.java
@@ -9,7 +9,13 @@ package csheets.ext.image.ui;
 import csheets.CleanSheets;
 import csheets.ui.ctrl.BaseAction;
 import csheets.ui.ctrl.UIController;
+import csheets.ui.ext.TableDecorator;
+import csheets.ui.sheet.SpreadsheetTable;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Image;
 import java.awt.event.ActionEvent;
+import java.io.File;
 import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -25,6 +31,7 @@ import javax.swing.filechooser.FileNameExtensionFilter;
  */
 class InsertImageAction extends BaseAction {
     private JLabel imageLabel;
+    private Image im;
     protected UIController uiController;
     public InsertImageAction(UIController uiController) {
     }
@@ -47,10 +54,23 @@ class InsertImageAction extends BaseAction {
         int returnVal = chooser.showOpenDialog(null);
         if(returnVal == JFileChooser.APPROVE_OPTION) {
             System.out.println("You chose to open this file: " + chooser.getSelectedFile().getName());
-            final ImageIcon image=new ImageIcon(chooser.getSelectedFile().getAbsolutePath());
-            imageLabel= new JLabel(image);
-            imageLabel.setBounds(15, 225,image.getIconWidth(),image.getIconHeight());
-           
+//            final ImageIcon image=new ImageIcon(chooser.getSelectedFile().getAbsolutePath());
+//            imageLabel= new JLabel(image);
+//            imageLabel.setBounds(15, 225,image.getIconWidth(),image.getIconHeight());
+            File im2 = new File(chooser.getSelectedFile().getAbsolutePath());
+            try {
+                im = ImageIO.read(im2);
+            } catch (IOException ex) {
+                Logger.getLogger(InsertImageAction.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            TableDecorator t = new TableDecorator() {
+                @Override
+                public void decorate(Graphics g, SpreadsheetTable table) {
+                   g.drawImage(im,uiController.getActiveCell().getAddress().getColumn(),uiController.getActiveCell().getAddress().getRow() ,table );
+                  
+                }
+            };
+            t.setEnabled(true);
         }
     }
     
