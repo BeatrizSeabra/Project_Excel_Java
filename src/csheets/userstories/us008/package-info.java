70f6bd137fde24da0d852d80975e72d656fdc1d8
Pedro Silva <1120644@isep.ipp.pt>
2014-06-09 15:01:05 +0100

    technical doc ref#275

---
+++ src/csheets/userstories/us008/package-info.java
@@ -0,0 +1,20 @@
+/*
+ * Technical documentation regarding the user story 008: sort multiple collumns of cells. 
+ * <br/>
+ * <br/>
+ * 
+ * <b>Requirement</b><br/>
+ * Sort range of cells. Sorting a range (several columns) cells.
+ * <br/>
+ * <br/>
+ *  
+ * <b>S008a: Analysis</b><br/>
+ * 
+ * This use case is the continue of the US007 the previous use case was supposed to do the sort of string and numbers of one column
+ * Week 2 requires the function to order multiple cells, so we just need to configure thi snew method to sort 1 or more columns.
+ * The class method that will be called is SortActionAZ()or SortActionZA(), depending on order that user choose, that allow to order the collumn.
+ * That method will call a bunch of methods since order cells, to clean arrays and set cells.
+ * <br/>
+ * <br/>
+*/
+package csheets.userstories.us008;
