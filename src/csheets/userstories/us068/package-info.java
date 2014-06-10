e7386a260a452c7a497b0bf9d2dc6f843681ee3f
Diogo Moreira <1120339@isep.ipp.pt>
2014-06-10 20:15:53 +0100

    Design do US068 fixes issue #278

--- src/csheets/userstories/us068/package-info.java
+++ src/csheets/userstories/us068/package-info.java
@@ -17,7 +17,48 @@
  * Analise de como abrir um workbook diretamente atraves da lista apresentada.<br/>
  * <br/>
  * <br/>
+ * 
+ * <b>S068d: Design</b><br/>
+ * Para implementar este caso de uso e' necessario a utilizacao de uma thread que faz a pesquisa de ficheiros de forma paralela. A implementacao deste caso de uso e' em todo semelhante 'a implementacao do US067.<br/>
+ * A principal alteracao e' a de que a pesquisa e' feita atraves da thread e a UI e' imediatamente atualizada sempre que e' encontrado um novo ficheiro.<br/>
+ * Por fim, e' necessario implementar um listener em cada um dos ficheiros apresentados na JTable para que seja apresentada uma "amostra" do seu conteudo sempre que seja selecionado.
+ * <br/>
+ * <br/>
+ *<img src="../../../csheets/userstories/us068/doc-files/us068_design1.png"> 
+ *<br/>
+ *<br/>
+ *
+ * 
  * @author Diogo Moreira (1120339)
  */
+ /*
+*
+@startuml doc-files/us068_design1.png
+actor User
+participant FindWorkbooksFileAction as FWFA
+participant "<b>fc</b> : FileChooser" as FC
+participant "<b>findWF</b> : FindWorkbooksFiles" as FWF
+participant runThread as thread
+participant "ArrayList<File>" as AL
+participant "<b>windowLWF</b> : WindowListWorkbooksFiles" as WLWF
+  
+User -> FWFA : actionPerformed(ActionEvent event)
+FWFA -> FC : <b>fc</b> = new FileChooser()
+FWFA -> FC : <b>fc</b>.setFileSelectionMode(FileChooser.DIRECTORIES_ONLY)
+FWFA -> FC : <b>fc</b>.showDialog()
+...
+FWFA -> FWF : <b>findWF</b> = new FindWorkbooksFiles()
+FWFA -> FWF : <b>findWF</b>.findWorkbooksFiles(fc.getSelectedFile(), ".*\\.cls")
+FWF -> thread: run()
+...
+thread-> AL: <b>listingFiles</b> = new ArrayList<File>
+
+loop for File <b>fileDirectory</b> : sub
+   thread-> thread: <b>listingFiles</b>.add(<b>fileDirectory</b>)
+   thread-> WLWF : <b>windowLWF</b> = new WindowListWorkbooksFiles(listingF)
+end
+@enduml
+*
+*/
 
 package csheets.userstories.us068;
