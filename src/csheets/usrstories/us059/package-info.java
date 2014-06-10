9ef03ff2abf407ab65ca8490a17264848729d2b5
1120546LuisAbreu <1120546@isep.ipp.pt>
2014-06-10 22:51:22 +0100

    S059d - Design

--- src/csheets/usrstories/us059/package-info.java
+++ src/csheets/usrstories/us059/package-info.java
@@ -8,15 +8,17 @@
  * <br/>
  *  
  * <b>S059a: Analysis</b><br/>
- * Menus novos com macros. Permitir associar a execução de macros às opções de menu novas. -Aquando da criação de um novo menu o utilizador pode escolher associar esse menu à execução de uma macro já existente. Para isso o utilizador aquando da criação do menu precisa de seleccionar a opção de associação do menu que vai criar a uma macro especifica.
-
- * 
+ *  
  * This functionality will allow the user to create new menus and use them to run previously created macros
  * 
  * 
  * <br/>
  * 
  * <b>S059d: Design</b><br/>
+ * 
+ * The menu creation window pops up and when filling out the data to create the <br/>
+ * new menu option the user will have an option to bind the execution to a specified macro <br/>
+ * <img src="../../../csheets/userstories/us059/doc-files/us059_design1.png">
  * 
  * <br/>
  * 
@@ -32,6 +34,31 @@
  * <br/>
  * 
  * @author 1120546
+ * 
+ * @startuml doc-files/us059_design1.png
+ participant "uic : UIController" as UIC
+ participant ExtensionManager as ExtM
+ participant "extension : MenuExtension" as EM
+ participant "uiExtension : EdMenuUIExtension" as UIEd
+ participant "aux1 : MenuEdWindow" as MEW
+ participant "uiext : UIExtension" as uext
+ participant "jMenu : JMenu" as jm
+ UIC -> ExtM : extensions=getExtensions();
+  loop for Extension ext : extensions
+ 	UIC -> EM : uiExtension=getUIExtension(this);
+ 	EM -> UIEd : new(extension, uic)
+ 	UIEd -> UIEd : getMenuItem();
+        activate UIEd
+ 	create MEW
+ 	UIEd -> MEW :  new (uic)
+        deactivate UIEd
+  	MEW -> uext : getMenu()
+  	MEW -> uext : selectMacro()
+  	uext -> jm : bindMacroToMenu(true)
+        uext -> jm : add(i)
+ 	UIC -> UIC : uiExtensions.add(uiExtension);
+ end
+  @enduml
  */
 /*
  *
