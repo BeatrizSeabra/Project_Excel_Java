cbde7086ce63871d586367ff46a5b9d3b6c2afee
stefanteixeira <1120739@isep.ipp.pt>
2014-06-11 16:31:46 +0100

    Função Evset feita a precisar de modificações nos argumentos. Classe AtributeFormula para associar formula ao evento criada também. ref #252

---
+++ src/csheets/ext/logfile/AtributeFormula.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package csheets.ext.logfile;
+
+import csheets.CleanSheets;
+import csheets.ui.ctrl.UIController;
+
+/**
+ *
+ * @author Stefan Parker
+ */
+public class AtributeFormula {
+    
+    String evento,formula;
+    public boolean activo=false;
+    public CleanSheets cs;
+    
+    public AtributeFormula(CleanSheets cs){
+		this.cs = cs;
+	}
+    
+    public AtributeFormula(String evento, String formula) {
+        setEvento(evento);
+        setFormula(formula);
+    }
+
+    public String getEvento() {
+        return evento;
+    }
+
+    public String getFormula() {
+        return formula;
+    }
+
+    public boolean isActivo() {
+        return activo;
+    }
+
+    public void setEvento(String evento) {
+        this.evento = evento;
+        activo=true;
+    }
+
+    public void setFormula(String formula) {
+        this.formula = formula;
+    }
+    
+    
+    
+}
