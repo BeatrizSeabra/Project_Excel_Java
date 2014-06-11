a34a0dbc1e9557747cfc52cbebabcff1deca32e2
1120564MarcoEsteves <1120564@isep.ipp.pt>
2014-06-11 17:17:18 +0100

    US043c - code (incomplete) and  update javadoc - ref #317

--- src/csheets/ext/runMacro/ui/runMacroAction.java
+++ src/csheets/ext/runMacro/ui/runMacroAction.java
@@ -5,6 +5,7 @@ import csheets.ui.ctrl.BaseAction;
 import csheets.ui.ctrl.UIController;
 import java.awt.Dimension;
 import javax.swing.JFrame;
+import csheets.ui.ctrl.FocusOwnerAction;
 
 /**
  * @author 1120564MarcoEsteves
@@ -36,6 +37,7 @@ public class runMacroAction extends BaseAction {
 
     public void actionPerformed(ActionEvent event) {
         JDialogRunMacro p= new JDialogRunMacro(runMacro,true);
+        p.setTitle("RUN MACRO");
         p.setVisible(true);
         p.setResizable(enabled);
         p.setMinimumSize(new Dimension(40, 40));
