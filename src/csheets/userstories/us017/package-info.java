6cc8f3e1bd6b9fa859f2bdd6815a601a1864e875
1110506 <1110506@isep.ipp.pt>
2014-06-12 16:08:06 +0100

    US017 - Analysis fixed issue #343

---
+++ src/csheets/userstories/us017/package-info.java
@@ -0,0 +1,45 @@
+/**
+* Technical documentation regarding the user story 017: Enviar Email<br/> 
+* <br/>
+* <br/>
+* 
+* <b>Requirement</b><br/>
+* Acrescentar uma janela que permite enviar um email. Deve ser possível <br/>
+* indicar os destinatários do email, o assunto e a mensagem. Deve ser possível anexar um ﬁcheiro. <br/>
+* <br/>
+* <br/>
+*  
+* <b>S016a: Analysis</b><br/>
+* Escolher o local onde vai ser implementada a funcionalidade. Como e uma extensão, será colocada no Menu Extensions->Email. <br/>
+* Vou ter de fazer uma nova UI para o utilizador escrever os destinatários para onde quero enviar o email, o assunto e a mensagem. Pode ou não anexar-se <br/>
+* um ficheiro <br/>
+* dividir os emails da jtextfield por ";" e depois coloca-los num arraylist para depois fazer um ciclo for que em cada iteracao enviar o mesmo email para um email diferente. <br/>
+* <br/>
+* 
+* <b>S016d: Design</b><br/>
+* <br/>
+* <br/>
+* 
+* <b>S016c: Coding</b><br/>
+* <br/>
+* <br/>
+* 
+* <b>S016u: Unit Tests</b><br/>
+ * <br/>
+* <br/>
+* <br/>
+* 
+* <b>S016f: Functional Tests</b><br/>
+* <br/>
+* <br/>
+* 
+* @author Rui 1110506
+*/
+/*
+*
+ @startuml doc-files/us017_design1.png
+
+ @enduml
+*
+*/
+package csheets.userstories.us017;
