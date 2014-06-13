79f06a78d1d6e9b7eaaf040adfeed6af4ccef8b3
RafaelChaves <1120677@isep.ipp.pt>
2014-06-13 10:05:19 +0100

    Correção de erros#3 ref#290

--- src/csheets/ext/findinstances/Cliente.java
+++ src/csheets/ext/findinstances/Cliente.java
@@ -3,79 +3,75 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package csheets.ext.findinstances;
 
 import java.io.IOException;
 import java.net.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.JOptionPane;
 
 /**
  *
  * @author RafaelChaves
  */
-public class Cliente extends Thread{
-    boolean running =true;
+public class Cliente extends Thread {
+
+    boolean running = true;
     DatagramSocket socket;
-    
-    public Cliente()
-    {
-        try{
-         socket = new DatagramSocket(9876); 
+
+    public Cliente() throws BindException {
+        try {
+            socket = new DatagramSocket(9876);
         } catch (SocketException ex1) {
-                    Logger.getLogger(Cliente.class.getName()).log(Level.SEVERE, null, ex1);
+            JOptionPane.showMessageDialog(null, "Error: Client already running");
         }
     }
-    public void run(){
 
-        boolean received = false;
-        
-        while(running)                
-        {
-            try{
-            
-            socket.setReuseAddress(true);
-            byte[] receiveData = new byte[1024];                 
-            DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);                   
-            socket.receive(receivePacket);                  
-            System.out.println("Recebi Broadcast");                   
-            InetAddress IPAddress = receivePacket.getAddress();                
-            String resp = "Eu";                
-            byte[] sendData = resp.getBytes();                   
-            DatagramPacket sendPacket =                   
-                    new DatagramPacket(sendData, sendData.length, IPAddress, 9877);                   
-            socket.send(sendPacket);
+    public void run() {
+        while (running) {
+            try {
+                socket.setBroadcast(true);
+                socket.setSoTimeout(0);
+                boolean received = false;
+                socket.setReuseAddress(true);
+                byte[] receiveData = new byte[1024];
+                DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+                socket.receive(receivePacket);
+                System.out.println("Recebi Broadcast");
+                InetAddress IPAddress = receivePacket.getAddress();
+                String resp = "Eu";
+                byte[] sendData = resp.getBytes();
+                DatagramPacket sendPacket =
+                        new DatagramPacket(sendData, sendData.length, IPAddress, 9877);
+                socket.send(sendPacket);
 
-            socket.setSoTimeout(10000);
+                socket.setSoTimeout(10000);
 
                 while (!received) {
                     try {
                         socket.receive(receivePacket);
                         System.out.println("Recebi sucesso do serv");
                         received = true;
-                        running = false;
                     } catch (SocketTimeoutException ex) {
                         System.out.println("A tentar enviar outra vez o ip");
                         receivePacket = new DatagramPacket(sendData, sendData.length, IPAddress, 9877);
                         socket.send(receivePacket);
                     }
                 }
-            
+
             } catch (IOException ex) {
                 Logger.getLogger(Cliente.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
     }
-    
-    
+
     public void startRunning() {
         running = true;
-        start();
     }
 
     public void stopRunning() {
         running = false;
+        socket.close();
     }
-    
 }
