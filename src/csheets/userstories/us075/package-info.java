1969bb7871075b68e45ea17dc23a5922ee9abcd2
stefanteixeira <1120739@isep.ipp.pt>
2014-06-17 16:23:10 +0100

    Design concluido fix #422

--- src/csheets/userstories/us075/package-info.java
+++ src/csheets/userstories/us075/package-info.java
@@ -9,32 +9,52 @@
 * <br/>
 * <br/>
 *  
-* <b>S007a: Analysis</b><br/>
+* <b>S075a: Analysis</b><br/>
 * It will be possible to, whenever that the cell changed, and if the file is already created (that means that the link needs to be established), the content is updated in the text file.
 * For that there will be a thread and maybe an infinite cicle that makes the thread and exportation never ends.
 * That way if a cell is changed, there will be an exportation of that modification.
 * <br/>
 * <br/>
 * 
-* <b>S007d: Design</b><br/>
-* 
+* <b>S075d: Design</b><br/>
+* To realize this userstory first we need to call the method to write to the file in the action performed. That still needs to be done at same time the program runs, so we still need the thread to run both of them at same time.
+* After the file is created, there will be a second writing file method that write everytime a cell change. For that, there is a boolean on StopCellEditing of CellEditor that everytime that method occurs it turns true.
+* After the writing of the changed content, the boolean returns to false again and continue to search.
+* <br/>
+* <br/>
+* <img src="../../../csheets/userstories/us075/doc-files/us075_design1.png"> 
 * <br/>
 * <br/>
 * 
-* * <b>S007c: Coding</b><br/>
+* * <b>S075c: Coding</b><br/>
 
 * <br/>
 * <br/>
 * 
-* <b>S007u: Unit Tests</b><br/>
+* <b>S075u: Unit Tests</b><br/>
 
 * <br/>
 * <br/>
-* * <b>S007f: Functional Tests</b><br/>
+* * <b>S075f: Functional Tests</b><br/>
 
 * <br/>
 * <br/>
 * 
+*/
+/*@startuml doc-files/us075_design1.png
+   actor User
+   User -> ExtensionTxt : Export txt
+   ExtensionTxt->UIExtensionTxt: getUiExtension() 
+   UIExtensionTxt->TxtMenu: getMenu() 
+   TxtMenu->TxtAction: new TxtAction()
+   TxtAction->FileExporter: create(file)
+   FileExporter->TxtAction: ExportFile(file)
+   loop 
+       alt uiController.getModificado()
+           FileExporter->TxtAction: ExportFile2(file)  
+       end
+   end
+@enduml
 */
 
 package csheets.userstories.us075;
