4fd216e55785b9ab2aba42427aad8afc507fd22e
oyermak <1120622@isep.ipp.pt>
2014-06-18 16:56:23 +0100

    ref #368

--- src/csheets/ext/share/MultiShare.java
+++ src/csheets/ext/share/MultiShare.java
@@ -4,10 +4,133 @@
  */
 package csheets.ext.share;
 
+import javax.swing.DefaultListModel;
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.DefaultTreeModel;
+
 /**
  *
- * @author Oleg
+ * @author Oleg (1120622)
  */
 public class MultiShare {
     
+    private static MultiShare multiShare = new MultiShare();
+    private DefaultListModel modelServers;
+    private DefaultListModel modelClients;
+    private boolean visible;
+    private DefaultTreeModel server_on_lan;
+    
+    private MultiShare() {
+        modelServers = new DefaultListModel();
+        modelClients = new DefaultListModel();
+        visible=true;
+        server_on_lan =new DefaultTreeModel(new DefaultMutableTreeNode("Servers"));
+    }
+    
+    public static MultiShare getShares() {
+
+        return multiShare;
+    }
+    
+    public void addClient(Client client){
+        modelClients.addElement(client);
+    }
+    
+    public void addServer(Server server){
+        modelServers.addElement(server);
+    }
+    
+    public void stopServerConnection(int index) {
+
+        if (modelServers.get(index) !=null) {
+            try {
+                ((Server)modelServers.get(index)).stopServer();
+                modelServers.remove(index);
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
+
+    }
+    
+    /**
+     * Pára uma deternimada Partilha
+     * @param server 
+     */
+    public void stopServerConnection(Server server) {
+        int index= modelServers.indexOf(server);
+        if (index!=-1) {
+            try {
+                ((Server)modelServers.get(index)).stopServer();
+                modelServers.remove(index);
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }else System.out.println("não encontrou");
+
+    }
+    
+   
+    public void stopClientConnection(int index) {
+
+        if (modelClients.get(index) !=null) {
+            try {
+                ((Client)modelClients.get(index)).stopClient();
+                multiShare.getModelClients().remove(index);
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+     
+   
+    public void stopClientConnection(Client client) {
+        int index= modelClients.indexOf(client);
+        if (index != -1) {
+            try {
+                ((Client)modelClients.get(index)).stopClient();
+                multiShare.getModelClients().remove(index);
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
+
+    }
+    
+  
+    public DefaultListModel getModelServers() {
+        return modelServers;
+    }
+
+   
+    public void setModelServers(DefaultListModel model) {
+        this.modelServers = model;
+    }
+
+   
+    public DefaultListModel getModelClients() {
+        return modelClients;
+    }
+
+    public void setModelClients(DefaultListModel modelClients) {
+        this.modelClients = modelClients;
+    }
+
+    public boolean isVisible() {
+        return visible;
+    }
+
+    
+    public void setVisivle(boolean visible) {
+        this.visible = visible;
+    }
+
+    public DefaultTreeModel getServer_on_lan() {
+        return server_on_lan;
+    }
+
+    public void setServer_on_lan(DefaultTreeModel server_on_lan) {
+        this.server_on_lan = server_on_lan;
+    }
+  
 }
