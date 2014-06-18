a7ad05cadd65e6a131a9d86daf2e3be5a2fb6c38
Beatriz Ferreira <1120268@isep.ipp.pt>
2014-06-18 22:43:55 +0100

    S083c: Coding (ptI)

--- src/csheets/ext/findinstances/Cliente.java
+++ src/csheets/ext/findinstances/Cliente.java
@@ -5,6 +5,7 @@
  */
 package csheets.ext.findinstances;
 
+import csheets.ui.ctrl.UIController;
 import java.io.IOException;
 import java.net.*;
 import java.util.logging.Level;
@@ -15,13 +16,18 @@ import javax.swing.JOptionPane;
  *
  * @author RafaelChaves
  */
+
 public class Cliente extends Thread {
 
+    protected UIController uiController;
     boolean running = true;
     DatagramSocket socket;
 
     // Creates the socket with the port n 9876 and listen to every menssage sent to his ip
-    public Cliente() throws BindException {
+    public Cliente(UIController uiController) throws BindException {
+        
+        this.uiController=uiController;
+        
         try {
             socket = new DatagramSocket(9876,InetAddress.getByName("0.0.0.0"));
             socket.setBroadcast(true);
@@ -30,6 +36,12 @@ public class Cliente extends Thread {
         } catch (UnknownHostException ex) {
             Logger.getLogger(Cliente.class.getName()).log(Level.SEVERE, null, ex);
         } 
+    }
+    
+    public static String getInfoString(){
+        String res="";
+        
+        return res;
     }
 
     public void run() {
