b3f0609f190aacca31abfda8bc1f2a00154159db
stefanteixeira <1120739@isep.ipp.pt>
2014-06-19 15:47:38 +0100

    Documentation Changed ref #427

--- src/csheets/ext/exporttxt/ui/TxtAction.java
+++ src/csheets/ext/exporttxt/ui/TxtAction.java
@@ -26,19 +26,19 @@ import javax.swing.filechooser.FileNameExtensionFilter;
 /**
  *
  * @author JSilva314 - Joao Paulo Silva/ Stefan Parker
- * 
+ *
  */
-public class TxtAction extends BaseAction{
+public class TxtAction extends BaseAction {
 
     /**
      * The user interface controller
      */
     protected UIController uiController;
-    
-    public String SEPARATOR = ",";
-    public String HEADER ="";
-    public String caminho = "";
-        
+
+    private String SEPARATOR = ",";
+    private String HEADER = "";
+    private String caminho = "";
+
     /**
      * Creates a new action.
      *
@@ -47,17 +47,19 @@ public class TxtAction extends BaseAction{
     public TxtAction(UIController uiController) {
         uiController.setExportStatus(false);
         this.uiController = uiController;
-                
+
     }
+
     /**
-     * 
-     * @return 
+     *
+     * @return
      */
     protected String getName() {
         return "TXT";
     }
+
     /**
-     * 
+     *
      */
     protected void defineProperties() {
         setEnabled(true);
@@ -80,60 +82,70 @@ public class TxtAction extends BaseAction{
         //iniciar a thread
         thread.start();
     }
+
     /**
-     * Classe que implementa o Runnable para correr a Thread. Esta thread servirá para 
-     * o programa correr em paralelo com o Export
+     * Class that implements Runnable to run the Thread. This thread is used to run the program in parallel to the thread.
      */
-     private class FileExporter implements Runnable{
+    private class FileExporter implements Runnable {
+
         File file;
-        public FileExporter(File file){
+
+        public FileExporter(File file) {
             this.file = file;
         }
+
         @Override
         public void run() {
             exportFile(file);
             addListeners();
-            while(uiController.getExportStatus()){
+            while (uiController.getExportStatus()) {
                 try {
                     Thread.sleep(500);
                 } catch (InterruptedException ex) {
                     Logger.getLogger(TxtAction.class.getName()).log(Level.SEVERE, null, ex);
                 }
-                if(uiController.getModificado()){
+                if (uiController.getModificado()) {
                     exportFileWithParameters(file);
                 }
-                
+
             }
         }
-      
+
     }
     
+    /**
+     * Method to write the file. This method it's called only when the export button is pressed.
+     * After this method occurs, it occurs again only when the button is pressed again. Otherwise, the method that will be called is the ExportFileWithParameters that contains the file path, separator and header.
+     * @param file 
+     */
+
     private void exportFile(File file) {
         int c = this.uiController.getActiveSpreadsheet().getColumnCount();
         int r = this.uiController.getActiveSpreadsheet().getRowCount();
-           
+
         try {
 
             JFileChooser fc = new JFileChooser();
-          
+
             fc.setFileFilter(new FileNameExtensionFilter("Ficheiro TXT (.txt)", "txt"));
-            
+
             // USER OPTIONS FOR SEPARATOR AND HEADER
-            do{
-            SEPARATOR = JOptionPane.showInputDialog("Choose Separator");
-            if (SEPARATOR.length()>1 || SEPARATOR.length()<1)
-                JOptionPane.showMessageDialog(null,"Invalid format, only 1 char","ERROR",JOptionPane.ERROR_MESSAGE);
-            }while (SEPARATOR.length() > 1 || SEPARATOR.length()<1);
+            do {
+                SEPARATOR = JOptionPane.showInputDialog("Choose Separator");
+                if (SEPARATOR.length() > 1 || SEPARATOR.length() < 1) {
+                    JOptionPane.showMessageDialog(null, "Invalid format, only 1 char", "ERROR", JOptionPane.ERROR_MESSAGE);
+                }
+            } while (SEPARATOR.length() > 1 || SEPARATOR.length() < 1);
             HEADER = JOptionPane.showInputDialog("Header text");
-                                 
+
             int retorno = fc.showSaveDialog(null);
             if (retorno == JFileChooser.APPROVE_OPTION) {
                 caminho = fc.getSelectedFile().getAbsolutePath();
             }
-         
+
             PrintWriter wr;
             wr = new PrintWriter(new FileWriter(caminho + ".txt"));
-            wr.println(SEPARATOR+HEADER);
+            wr.println(SEPARATOR + HEADER);
             for (int row = 0; row < r + 1; row++) {
                 for (int column = 0; column < c + 1; column++) {
                     wr.print(this.uiController.getActiveSpreadsheet().getCell(column, row).getContent() + SEPARATOR);
@@ -150,23 +162,24 @@ public class TxtAction extends BaseAction{
         }
 
     }
-    
-    
-    /** 
+
+    /**
      * A method to write on the file previous created. If this method is called
-     * means that the export action already was called once and already is created a file with content.
-     * So the class verifies if there any changed cell and calls this method to write on the file.
-     * @param file 
+     * means that the export action already was called once and already is
+     * created a file with content. So the class verifies if there any changed
+     * cell and calls this method to write on the file.
+     *
+     * @param file
      */
-	 
-     private void exportFileWithParameters(File file){
+    
+    private void exportFileWithParameters(File file) {
         int c = this.uiController.getActiveSpreadsheet().getColumnCount();
         int r = this.uiController.getActiveSpreadsheet().getRowCount();
-         
-         try {
+
+        try {
             PrintWriter wr;
             wr = new PrintWriter(new FileWriter(caminho + ".txt"));
-            wr.println(SEPARATOR+HEADER);
+            wr.println(SEPARATOR + HEADER);
             for (int row = 0; row < r + 1; row++) {
                 for (int column = 0; column < c + 1; column++) {
                     wr.print(this.uiController.getActiveSpreadsheet().getCell(column, row).getContent() + SEPARATOR);
@@ -181,14 +194,14 @@ public class TxtAction extends BaseAction{
         }
 
     }
-     
-        /**
-         * Implements cell listeners on the active spreedsheet. This class implements cell 
-         * listeners to all of the cell on the active spreedsheet, so if any cell change,
-         * it will set boolean true and write on the file
-         */
-	
-	class OnChangeListener implements CellListener {
+
+    /**
+     * Implements cell listeners on the active spreedsheet. This class
+     * implements cell listeners to all of the cell on the active spreedsheet,
+     * so if any cell change, it will set boolean true and write on the file
+     */
+    
+    class OnChangeListener implements CellListener {
 
         @Override
         public void valueChanged(Cell cell) {
@@ -214,20 +227,18 @@ public class TxtAction extends BaseAction{
         public void cellCopied(Cell cell, Cell source) {
             throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
         }
-	 }
-	 
-	 /**
-          * This method calls the OnChangeListener to set Cell Listeners to all of the cells.
-          */
-        
-        
-	 public void addListeners(){
-	 for (int i=0;i<52;i++){
-		for(int j=0;j<128;j++){
-			
-                        Cell cell = uiController.getActiveSpreadsheet().getCell(i, j);
-			cell.addCellListener(new OnChangeListener());
-			}
-		}
-	}
+    }
+
+    /**
+     * This method calls the OnChangeListener to set Cell Listeners to all of the cells.
+     */
+    public void addListeners() {
+        for (int i = 0; i < 52; i++) {
+            for (int j = 0; j < 128; j++) {
+
+                Cell cell = uiController.getActiveSpreadsheet().getCell(i, j);
+                cell.addCellListener(new OnChangeListener());
+            }
+        }
+    }
 }
