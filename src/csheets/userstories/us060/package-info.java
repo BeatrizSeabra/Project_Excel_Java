52d1b612337fe1ccb9eeb0c3ec7d928aeafe61ff
Tiago_Mitch <1090675@isep.ipp.pt>
2014-06-20 05:35:00 +0100

    S060d: ref #437  US060 - Estender menus com macros  - Adicionada ao package-info.java com informação relativo à tarefa de design, para explicar os requisitos e análise elaborados para esta user storie;  - Diagrama de sequência (SD) que ilustra os requisitos e a análise de suporte à implementação desta user storie.

--- src/csheets/userstories/us060/package-info.java
+++ src/csheets/userstories/us060/package-info.java
@@ -28,7 +28,21 @@
  * <br/>
  * 
  * <b>S060d: Design</b><br/>
- * 
+ * Firstly analyzed the diagrams used in the user stories US059 and US058,<br/>
+ * to understand the process used to analyze these features, which depend on the<br/> 
+ * implementation of user storie US060. After analysis it was concluded that in<br/> 
+ * order to run a macro, would need to be implemented the user storie US044, to<br/> 
+ * allow the user to run an macro on the menu item of cleansheets application.<br/>
+ * After two days of intensive analysis and design of a solution to the requirement<br/> 
+ * of membership options and attempts to actions menu, so that it was possible to<br/> 
+ * control the actions of the menu dynamically, so as not to escape of the best<br/> 
+ * practicals of the object-oriented programming. Unable to find an analysis of<br/> 
+ * design for the desired requirement on instead of running the macro associated<br/> 
+ * with a menu item already created the cleansheets, was then associated with a<br/> 
+ * new menu item being created on the menu that performs the operation of the<br/> 
+ * choice of the macro to be executed on the cleansheets application. The following<br/> 
+ * sequence diagram shows the flow of the implementation of this user storie.<br/>
+ * <img src="../../../csheets/userstories/us101/doc-files/us060_design1_sequence_diagram.png">
  * <br/>
  * <br/>
  * 
@@ -47,6 +61,87 @@
  * <br/>
  * 
  * @author 1090675 - Tiago Pereira
+ */
+/*
+ *
+    @startuml doc-files/us060_design1_sequence_diagram.png
+        hide footbox
+        skinparam backgroundColor #EEEBDC
+
+        skinparam sequence {
+                ArrowColor DeepSkyBlue
+                ActorBorderColor DeepSkyBlue
+                LifeLineBorderColor blue
+                LifeLineBackgroundColor #A9DCDF
+
+                ParticipantBorderColor DeepSkyBlue
+                ParticipantBackgroundColor DodgerBlue
+                ParticipantFontName Impact
+                ParticipantFontSize 17
+                ParticipantFontColor #A9DCDF
+
+                ActorBackgroundColor blue
+                ActorFontColor DeepSkyBlue
+                ActorFontSize 17
+                ActorFontName Aapex
+        }
+        actor User
+          participant "<b>wmtmi</b> : WindowMacroToMenuItem" as WMTMI
+          participant "<b>uiController</b> : UIController" as UIC
+          participant "ArrayList<>" as L
+          participant "<b>app</b> : CleanSheets" as CS
+          participant "<b>frame</b> : csheets.ui.Frame" as F
+          participant "<b>jCb</b> : JComboBox" as JCB
+          participant "<b>uiExtension<b> : UIExtensionas" as UIE
+          participant "<b>itemMenu</b> : JMenuItem" as JMI
+          participant "<b>actionListener</b> : ActionListener" as AL
+          participant "JButton<>" as JB   
+          participant "<b>macro</b> : Macro" as M
+          participant ExtensionManager as ExtM
+          participant "extension : MenuExtension" as EM
+          participant "uiExtension : UIExtension" as UIE
+            User -> WMTMI : actionPerformed(ActionEvent event)
+            WMTMI -> L : arrayMenuItem = new ArrayList()
+            WMTMI -> L : uiController.getActiveWorkbook().getMacros()
+            WMTMI -> CS : new CleanSheets()
+            WMTMI -> F : new csheets.ui.Frame(app) 
+            ... 
+            WMTMI -> WMTMI : formWindowActivated(WindowEvent)
+            activate WMTMI
+                 loop for (Macro macro : arrayMacros)       
+                     WMTMI -> JCB : CbMacroRun.addItem(macro)
+                 end   
+            deactivate WMTMI
+            ...
+            WMTMI -> WMTMI : getMenuItemsOfApplication(frame.getJMenuBar())
+            activate WMTMI
+                 loop for(JMenuItem jMenuItem : arrayMenuItem)
+                     WMTMI -> JCB : CbMenuItem.addItem(jMenuItem)
+                 end   
+            deactivate WMTMI
+            ...
+            WMTMI -> JB : btnOKActionPerformed()
+            WMTMI -> M : new Macro()       
+            UIC -> ExtM : extensions=getExtensions();
+            loop for UIExtension uiExtension : listExtension
+               UIC -> EM : uiExtension=getUIExtension(this);
+               JB -> JMI : new JMenuItem
+               JB -> AL : actionListener = new ActionListener()
+               M -> AL : m.compiler()
+               M -> AL : m.results()
+               ...
+               JMI -> AL : itemMenu.addActionListener(actionListener)
+               JB -> UIE : uiExtension.getMenu().add(itemMenu)
+            end
+         UIC -> User
+         note left
+             show to user the result
+             of the running of the macro
+             associated to the menu item 
+             selection on the cleansheets
+         end note
+    @enduml
+ *
  */
 
 package csheets.userstories.us060;
